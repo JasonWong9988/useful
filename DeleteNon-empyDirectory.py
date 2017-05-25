35e983e9f05cc02a73a12f21697355734c1a5c4b
Jason Wong <wonglenchong@163.com>
2017-05-25 22:05:31 +0800

    done

---
+++ DeleteNon-empyDirectory.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-05-25 07:44:35
+# @Author  : Jason Wong (wonglenchong@163.com)
+# @Link    : https://github.com/JasonWong9988/useful
+# @Version : $Id$
+
+import os, shutil
+
+def delNon_emptyDir(dir_):
+	if os.path.isdir(dir_): # 判断dir_是否是一个目录
+		file_list = os.listdir(dir_) # 获取目录中的所有文件或文件夹
+		for file in file_list:
+			temp_dir = os.path.join(dir_, file)
+			if os.path.isfile(temp_dir):
+				if temp_dir[-4:] in ['.int', '.sys', '.dll', '.adt']:
+					pass
+				else:
+					os.remove(temp_dir)
+			if os.path.isdir(temp_dir):
+				shutil.rmtree(temp_dir)
+
+	else:
+		raise FileNotFoundError("系统找不到指定的路径：%s" % dir_)
+
+dir_ = "D:\KuGou"
+delNon_emptyDir(dir_)
