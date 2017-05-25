606e41f5e81d307ed5ef1e71db839fe70ceb0050
Jason Wong <wonglenchong@163.com>
2017-05-25 22:43:14 +0800

    pass --> continue

--- DeleteNon-empyDirectory.py
+++ DeleteNon-empyDirectory.py
@@ -13,13 +13,15 @@ def delNon_emptyDir(dir_):
 		for file in file_list:
 			temp_dir = os.path.join(dir_, file)
 			if os.path.isfile(temp_dir):
-				if temp_dir[-4:] in ['.int', '.sys', '.dll', '.adt']:
-					pass
-				else:
-					os.remove(temp_dir)
+				try:
+					if temp_dir[-4:].lower() in ['.int', '.sys', '.dll', '.adt']:
+						continue
+					else:
+						os.remove(temp_dir)
+				except os.error:
+					autoRun.exception("remove %s error" % temp_dir)
 			if os.path.isdir(temp_dir):
 				shutil.rmtree(temp_dir)
-
 	else:
 		raise FileNotFoundError("系统找不到指定的路径：%s" % dir_)
 
